3bbffa2d97c91cae949e83d1a666939240d8e7a7
jakob <ji64@njit.edu>
2022-02-21 15:42:19 -0500

    Added subtraction, multiplication, and division

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,8 +1,22 @@
 """ This is the Calculator Class"""
+
+
 class Calculator:
     """ This is the default result property"""
     result = 0
 
-    def add(self,x):
-        self.result = self.result + x
+    def add(self, x):
+        self.result += x
+        return self.result
+
+    def subtract(self, x):
+        self.result -= x
+        return self.result
+
+    def multiply(self, x):
+        self.result *= x
+        return self.result
+
+    def divide(self, x):
+        self.result /= x
         return self.result
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,13 +2,15 @@
 import pytest
 from calculator import Calculator
 
+
 def test_calculator_is_instance():
-    #instantiating the Calculator Class
+    # instantiating the Calculator Class
     calculator = Calculator()
     assert isinstance(calculator, Calculator)
 
+
 def test_calculator_result_property():
-    #instantiating the Calculator Class
+    # instantiating the Calculator Class
     calc1 = Calculator()
     calc2 = Calculator()
     calc1.result = 5
@@ -16,7 +18,28 @@ def test_calculator_result_property():
     assert calc1.result == 5
     assert calc2.result == 6
 
+
 def test_calculator_add_method():
-    #instantiating the Calculator Class
+    # instantiating the Calculator Class
     calculator = Calculator()
     assert calculator.add(1) == 1
+
+
+def test_calculator_subtract_method():
+    # instantiating the Calculator Class
+    calculator = Calculator()
+    assert calculator.subtract(1) == -1
+
+
+def test_calculator_multiply_method():
+    # instantiating the Calculator Class
+    calculator = Calculator()
+    calculator.result = 2
+    assert calculator.multiply(3) == 6
+
+
+def test_calculator_divide_method():
+    # instantiating the Calculator Class
+    calculator = Calculator()
+    calculator.result = 2
+    assert calculator.divide(2) == 1
